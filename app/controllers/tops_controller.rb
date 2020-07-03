d327be1bd7cc31b299928d8a5b81d68642012816
いぬい <inui@inuinoMacBook-Pro.local>
2020-07-03 20:11:24 +0900

    7_3

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,4 +4,10 @@ class TopsController < ApplicationController
 
   def help
   end
+
+  def about
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'tops#home'
   get 'help', to: 'tops#help'
-  
-resources :users
+  get 'about', to: 'tops#about'
+  get 'contact', to: 'tops#contact'
+  get '/signup', to: 'users#new'
+
+  resources :users, except:[:new]
 
 end
